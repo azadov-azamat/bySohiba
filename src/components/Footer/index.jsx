e4ad99c1a333e0d207daa99612086f055bd456c1
azamat_azadov <azamatazadov112@gmail.com>
2022-09-02 00:55:11 +0500

    edited

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,13 +14,6 @@ export default function Footer(){
         dispatch(handleSetId(0))
     },[])
 
-   function changeLocation(id) {
-       dispatch(handleSetId(id))
-   }
-
-    console.log(filial[activeId])
-    console.log(activeId)
-
     return(
         <footer className={classes.footer}>
             <Title text={"наш адрес"}/>
@@ -28,27 +21,28 @@ export default function Footer(){
                <div className={classes['footer-top']}>
                    <div className={classes.tex1}>
                        <div className={classes.nav}>
-                           <span>Салон Чорсу</span>
+                           {filial.map((item, index) => <div key={item?.id} className={activeId === index ? classes.active : classes['not-active']}
+                                                             onClick={()=> activeId !== index && dispatch(handleSetId(index))}>{item?.name}</div>)}
                        </div>
                        <div className={classes.data}>
                            <div className={classes.text}>
                                <span className={classes.p}>Адрес</span>
-                               <span>Ташкент, улица Лайлитог 97</span>
-                               <span>Ориентр реторан Мумтоз</span>
+                               <span>{filial[activeId]?.address}</span>
+                               <span>Ориентр: {filial[activeId]?.orient}</span>
                            </div>
                            <div className={classes.text}>
                                <span className={classes.p}>Время работы</span>
-                               <span>Без выходных 10:00 - 19:00</span>
+                               <span>{filial[activeId]?.workTime}</span>
                            </div>
                            <div className={classes.text}>
                                <span className={classes.p}>Номер телефона</span>
-                               <span><a href="tel:+998 97 101 88-80">+998 97 101 88-80</a></span>
+                               <span><a href={`tel:${filial[activeId]?.phoneNumber}`}>{filial[activeId]?.phoneNumber}</a></span>
                            </div>
                        </div>
                        <div className={classes.social}>
                            <ul>
-                               {social.map((item, index) => <li key={index.toString()}><a href={item.link}
-                                                                                          target={"_blank"}>{item.name}</a></li>)}
+                               {social.map((item, index) => <li key={index.toString()}><a href={item?.link}
+                                                                                          target={"_blank"}>{item?.name}</a></li>)}
                            </ul>
                        </div>
                    </div>
